688cc931f9494ac2f50a58c4738a8953afaaaf88
Rui Pinge <rui@pinge.org>
2021-03-13 18:47:40 +0000

    chore(monitoring): tracing and performance support with sentry

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import './App.scss';
+
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    dsn:
+      'https://2e937d9ae4044696992e8d4afba8d9b5@o551236.ingest.sentry.io/5674476',
+    integrations: [new Integrations.BrowserTracing()],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0
+  });
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
