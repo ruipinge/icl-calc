4343e9dabaacef1e921ae3d666d79d8dc75f0126
Rui Pinge <rui@pinge.org>
2021-03-22 01:58:22 +0000

    fix(refactor): code smell fix

--- src/ICLContainer.tsx
+++ src/ICLContainer.tsx
@@ -1,15 +1,45 @@
+import { Formik, FormikState } from 'formik';
+import { ICLInputs, INITIAL_VALUES } from './types';
 import { Route, HashRouter as Router, Switch } from 'react-router-dom';
 
 import { Footer } from './misc/Footer';
-import { Formik } from 'formik';
 import { ICLSchema } from './ICLSchema';
-import { INITIAL_VALUES } from './types';
 import { Matrix } from './matrix';
 import { NavBar } from './misc/NavBar';
 import { Normality } from './normality';
 import { Patient } from './patient';
 import { Regression } from './regression';
 import { TabLinks } from './misc/TabLinks';
+
+const TabContent = ({
+  errors,
+  touched,
+  values,
+  ...otherProps
+}: FormikState<ICLInputs>) => (
+  <Switch>
+    <Route path="/normality">
+      <Normality />
+    </Route>
+    <Route path="/matrix">
+      <Matrix
+        ata={values.biometrics.ata}
+        clr={values.biometrics.clr * 0.001} // CLR in Matrix is in millimetres
+      />
+    </Route>
+    <Route path="/regression">
+      <Regression />
+    </Route>
+    <Route path="/">
+      <Patient
+        values={values}
+        errors={errors}
+        touched={touched}
+        {...otherProps}
+      />
+    </Route>
+  </Switch>
+);
 
 export const ICLContainer = () => {
   const initialValues = INITIAL_VALUES;
@@ -30,28 +60,12 @@ export const ICLContainer = () => {
             <Router hashType="noslash">
               <TabLinks />
               <hr />
-              <Switch>
-                <Route path="/normality">
-                  <Normality />
-                </Route>
-                <Route path="/matrix">
-                  <Matrix
-                    ata={values.biometrics.ata}
-                    clr={values.biometrics.clr * 0.001} // CLR in Matrix is in millimetres
-                  />
-                </Route>
-                <Route path="/regression">
-                  <Regression />
-                </Route>
-                <Route path="/">
-                  <Patient
-                    values={values}
-                    errors={errors}
-                    touched={touched}
-                    {...otherProps}
-                  />
-                </Route>
-              </Switch>
+              <TabContent
+                values={values}
+                errors={errors}
+                touched={touched}
+                {...otherProps}
+              />
             </Router>
           </div>
           <Footer />
