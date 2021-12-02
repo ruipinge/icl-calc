304dca2ec4db21cdfeea82ec0c427fff7535320c
Rui Pinge <rui@pinge.org>
2021-12-02 18:23:34 +0000

    chore(npm): dependencies and node.js upgrade

--- src/normality/Gauge.test.tsx
+++ src/normality/Gauge.test.tsx
@@ -2,34 +2,8 @@ import { Gauge, buildZones, quantile } from './Gauge';
 import { create, act as testAct } from 'react-test-renderer';
 
 const DATASET = [
-  1,
-  2,
-  2,
-  3,
-  3,
-  3,
-  4,
-  4,
-  4,
-  4,
-  5,
-  5,
-  5,
-  5,
-  5,
-  6,
-  6,
-  6,
-  6,
-  6,
-  6,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7,
-  7
+  1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5, 5, 5, 5, 6, 6, 6, 6, 6, 6, 7, 7, 7, 7, 7,
+  7, 7
 ];
 
 it('calculates quantile correctly', () => {
